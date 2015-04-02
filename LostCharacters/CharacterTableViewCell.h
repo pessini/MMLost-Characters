5e4e075f42e5557a0cd374464398b3b0ced6c6ca
Leandro Pessini <leandro@pessini.me>
2015-04-01 23:08:52 -0300

    Add a custom class for TableViewCell

---
+++ LostCharacters/CharacterTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  CharacterTableViewCell.h
+//  LostCharacters
+//
+//  Created by Leandro Pessini on 4/1/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *characterImage;
+@property (weak, nonatomic) IBOutlet UILabel *actorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *seatLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gender;
+
+@end
